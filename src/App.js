6492742c08986ec7fa039dc28941b5433206ec0f
Sannim <inegg.apps@gmail.com>
2020-07-23 20:42:02 +0900

    Todolist 프로젝트 생성 및 UI 구성(1)

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import TodoTemplate from "./Components/TodoTemplate";
+import TodoHead from "./Components/TodoHead";
+import TodoList from "./Components/TodoList";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background:#e9ecef;
+  }
+`;
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+      </TodoTemplate>
+    </>
+  );
+}
+
+export default App;
